ccd86de1bc5954307b406c6e35def29c5b62c246
Antti Möttönen <antti.moettoenen@gmail.com>
2016-01-23 17:20:51 +0200

    Minor formatting

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ void decode(const char *filename, unsigned& width, unsigned& height, vector<unsi
     unsigned error = lodepng::decode(image, width, height, filename);
 
     //if there's an error, display it
-    if(error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+    if (error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
 
     //the pixels are now in the vector "image", 4 bytes per pixel, ordered RGBARGBA..., use it as texture, draw it, ...
 }
