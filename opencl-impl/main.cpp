9e0b32d34aab3dcaea1d084ee6841a263b4897ed
Antti Möttönen <antti.moettoenen@gmail.com>
2017-01-06 15:18:54 +0200

    Oneway zncc in OpenCL

--- opencl-impl/main.cpp
+++ opencl-impl/main.cpp
@@ -12,13 +12,19 @@
 using std::vector;
 using std::cout;
 using std::endl;
-
+using std::string;
 
 
 struct Image {
     ::size_t height, width;
     vector<unsigned char> pixels;
 };
+
+struct imageSet {
+    Image originalImage;
+    cl::Image2D original, gs, meaned, znccd, output;
+    string fileName;
+} left, right;
 
 
 void decode(const char *filename, unsigned &width, unsigned &height, vector<unsigned char> &image) {
@@ -48,12 +54,15 @@ Image load_image(const char *filename) {
 int main(int argc, char *argv[]) {
     const char *left_name = argc > 1 ? argv[1] : "im0.png";
     const char *right_name = argc > 2 ? argv[2] : "im1.png";
+    left.fileName = "left.png";
+    right.fileName = "right.png";
 
-    Image left = load_image(left_name);
-    Image right = load_image(right_name);
+    left.originalImage = load_image(left_name);
+    right.originalImage = load_image(right_name);
+
     Image resizedImage = {};
-    resizedImage.width = left.width / 4;
-    resizedImage.height = left.height / 4;
+    resizedImage.width = left.originalImage.width / 4;
+    resizedImage.height = left.originalImage.height / 4;
 
     vector<cl::Platform> platforms;
     cl::Platform::get(&platforms);
@@ -104,18 +113,31 @@ int main(int argc, char *argv[]) {
 
     cl_int image_err;
 
-    cl::Image2D original;
-    cl::Image2D resized;
-    cl::Image2D meaned;
 
+    size_t h = left.originalImage.height, w = left.originalImage.width;
     try {
-        original = cl::Image2D(ctx, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, imageFormat, left.width,
-                               left.height, 0, &left.pixels[0], &image_err);
-        resized = cl::Image2D(ctx, CL_MEM_READ_WRITE, imageFormat, resizedImage.width, resizedImage.height, 0,
+        left.original = cl::Image2D(ctx, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, imageFormat, w,
+                                    h, 0, &left.originalImage.pixels[0], &image_err);
+        left.gs = cl::Image2D(ctx, CL_MEM_READ_WRITE, imageFormat, resizedImage.width, resizedImage.height, 0,
                               NULL, &image_err);
-        meaned = cl::Image2D(ctx, CL_MEM_READ_WRITE, imageFormat, resizedImage.width, resizedImage.height, 0,
-                             NULL, &image_err);
+        left.meaned = cl::Image2D(ctx, CL_MEM_READ_WRITE, imageFormat, resizedImage.width, resizedImage.height, 0,
+                                  NULL, &image_err);
+        left.znccd = cl::Image2D(ctx, CL_MEM_READ_WRITE, imageFormat, resizedImage.width, resizedImage.height, 0, NULL,
+                                 &image_err);
+        left.output = cl::Image2D(ctx, CL_MEM_WRITE_ONLY, imageFormat, resizedImage.width, resizedImage.height, 0, NULL,
+                                  &image_err);
 
+
+        right.original = cl::Image2D(ctx, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, imageFormat, w,
+                                     h, 0, &right.originalImage.pixels[0], &image_err);
+        right.gs = cl::Image2D(ctx, CL_MEM_READ_WRITE, imageFormat, resizedImage.width, resizedImage.height, 0,
+                               NULL, &image_err);
+        right.meaned = cl::Image2D(ctx, CL_MEM_READ_WRITE, imageFormat, resizedImage.width, resizedImage.height, 0,
+                                   NULL, &image_err);
+        right.znccd = cl::Image2D(ctx, CL_MEM_READ_WRITE, imageFormat, resizedImage.width, resizedImage.height, 0,
+                                  NULL, &image_err);
+        right.output = cl::Image2D(ctx, CL_MEM_WRITE_ONLY, imageFormat, resizedImage.width, resizedImage.height, 0,
+                                   NULL, &image_err);
     } catch (const cl::Error &ex) {
         std::cerr
         << "Error creating image " << endl << ex.what() << " " << ex.err() << endl << "Error " << image_err << endl;
@@ -129,69 +151,117 @@ int main(int argc, char *argv[]) {
 
     cl::Kernel resize(program, "resize");
     cl::Kernel mean(program, "calculate_mean");
+    cl::Kernel zncc(program, "calculate_zncc");
 
     try {
-        resize.setArg(0, left.height);
-        resize.setArg(1, left.width);
-        resize.setArg(2, original);
-        resize.setArg(3, resized);
-        resize.setArg(4, left.width * left.height);
-
-        mean.setArg(0, resized);
-        mean.setArg(1, meaned);
+        resize.setArg(0, h);
+        resize.setArg(1, w);
+        resize.setArg(4, h * w);
         mean.setArg(2, 4);
     } catch (const cl::Error &ex) {
         std::cerr << ex.what() << " " << ex.err() << endl;
         return 1;
     }
 
-    cout << "Putting kernel to work" << endl;
-    cl_int err;
-    cl::Event e1, e2;
+    for (auto set : {left, right}) {
+        try {
+            resize.setArg(2, set.original);
+            resize.setArg(3, set.gs);
+            mean.setArg(0, set.gs);
+            mean.setArg(1, set.meaned);
+        } catch (const cl::Error &ex) {
+            std::cerr << ex.what() << " " << ex.err() << endl;
+            return 1;
+        }
 
-    err = queue.enqueueNDRangeKernel(resize, cl::NullRange, cl::NDRange(left.width, left.height), cl::NullRange,
-                                     NULL, &e1);
-    vector<cl::Event> await = vector<cl::Event>();
-    await.push_back(e1);
-    err = queue.enqueueNDRangeKernel(mean, cl::NullRange, cl::NDRange(resizedImage.width, resizedImage.height),
-                                     cl::NullRange, &await, &e2);
-    if (err != CL_SUCCESS) {
-        cout << "Error in queue " << err << endl;
-        return 1;
+        cout << "Putting kernel to work" << endl;
+        cl_int err;
+        cl::Event e1, e2;
+
+        err = queue.enqueueNDRangeKernel(resize, cl::NullRange, cl::NDRange(w, h), cl::NullRange, NULL, &e1);
+        vector<cl::Event> await = vector<cl::Event>();
+        await.push_back(e1);
+        err = queue.enqueueNDRangeKernel(mean, cl::NullRange, cl::NDRange(resizedImage.width, resizedImage.height),
+                                         cl::NullRange, &await, &e2);
+        if (err != CL_SUCCESS) {
+            cout << "Error in queue " << err << endl;
+            return 1;
+        }
+        cout << "Kernel working" << endl;
+
+        vector<uint8_t> output(resizedImage.height * resizedImage.width * 4);
+
+        cl::size_t<3> start;
+        start[0] = 0;
+        start[1] = 0;
+        start[2] = 0;
+
+        cl::size_t<3> end;
+        end[0] = resizedImage.width;
+        end[1] = resizedImage.height;
+        end[2] = 1;
+
+        cout << "Waiting" << endl;
+        e2.wait();
+
+        cout << "Ready" << endl;
+
+        try {
+            err = queue.enqueueReadImage(set.meaned, CL_TRUE, start, end, 0, 0, &output[0], NULL, NULL);
+        } catch (const cl::Error &ex) {
+            std::cerr << "Error " << ex.what() << " code " << ex.err() << endl;
+            return 1;
+        }
+
+        if (err != CL_SUCCESS) {
+            cout << err << endl;
+            return 1;
+        }
+        cout << "Mem copy ready" << endl;
+        encode_to_disk(set.fileName.c_str(), output, unsigned(resizedImage.width), unsigned(resizedImage.height));
     }
-    cout << "Kernel working" << endl;
 
-    vector<uint8_t> output(resizedImage.height * resizedImage.width * 4);
+    zncc.setArg(5, 4);
+    zncc.setArg(6, 70);
 
-    cl::size_t<3> start;
-    start[0] = 0;
-    start[1] = 0;
-    start[2] = 0;
+    for (auto set : {left}) {
+        zncc.setArg(0, left.gs);
+        zncc.setArg(1, right.gs);
+        zncc.setArg(2, left.meaned);
+        zncc.setArg(3, right.meaned);
+        zncc.setArg(4, left.znccd);
 
-    cl::size_t<3> end;
-    end[0] = left.width / 4;
-    end[1] = left.height / 4;
-    end[2] = 1;
+        cl::Event e1;
+        int err = queue.enqueueNDRangeKernel(zncc, cl::NullRange,
+                                             cl::NDRange(resizedImage.width, resizedImage.height), cl::NullRange, NULL,
+                                             &e1);
+         vector<uint8_t> output(resizedImage.height * resizedImage.width * 4);
 
-    cout << "Waiting" << endl;
-    e2.wait();
-    cout << "Ready" << endl;
+        cl::size_t<3> start;
+        start[0] = 0;
+        start[1] = 0;
+        start[2] = 0;
 
-    try {
-        err = queue.enqueueReadImage(meaned, CL_TRUE, start, end, 0, 0, &output[0], NULL, NULL);
-    } catch (const cl::Error &ex) {
-        std::cerr << "Error " << ex.what() << " code " << ex.err() << endl;
-        return 1;
+        cl::size_t<3> end;
+        end[0] = resizedImage.width;
+        end[1] = resizedImage.height;
+        end[2] = 1;
+
+        e1.wait();
+        try {
+            err = queue.enqueueReadImage(set.znccd, CL_TRUE, start, end, 0, 0, &output[0], NULL, NULL);
+        } catch (const cl::Error &ex) {
+            std::cerr << "Error " << ex.what() << " code " << ex.err() << endl;
+            return 1;
+        }
+
+        if (err != CL_SUCCESS) {
+            cout << err << endl;
+            return 1;
+        }
+        cout << "Mem copy ready" << endl;
+        encode_to_disk((string("zncc_").append(set.fileName)).c_str(), output, unsigned(resizedImage.width), unsigned(resizedImage.height));
     }
-
-    if (err != CL_SUCCESS) {
-        cout << err << endl;
-        return 1;
-    }
-
-    cout << "Mem copy ready" << endl;
-
-    encode_to_disk("test.png", output, unsigned(resizedImage.width), unsigned(resizedImage.height));
 
     cout << "Bye" << endl;
 }
--- opencl-impl/resize.cl
+++ opencl-impl/resize.cl
@@ -40,3 +40,55 @@ __kernel void calculate_mean(
     uint4 pix = {val, val, val, 255};
     write_imageui(output, coord, pix);
 }
+
+
+__kernel void calculate_zncc(
+        __read_only image2d_t left,
+        __read_only image2d_t right,
+        __read_only image2d_t left_mean,
+        __read_only image2d_t right_mean,
+        __write_only image2d_t output,
+        int window_size,
+        int max_disp
+        ) {
+        sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_CLAMP_TO_EDGE | CLK_FILTER_LINEAR;
+        int x = get_global_id(0);
+        int y = get_global_id(1);
+        int2 coord = {x, y};
+
+        int l_mean = read_imageui(left_mean, sampler, coord).s0;
+        float lower_left_sum = 0;
+        uint best_disp = 0;
+        float best_zncc = 0;
+
+        for(int y1 = -window_size ; y1 <= window_size; y1++ ) {
+            for (int x1 = -window_size ; x1 <= window_size ; x1++ ){
+                int2 coord2 = {x+x1, y+y1};
+                int pix_val = read_imageui(left, sampler, coord2).s0 - l_mean;
+                lower_left_sum += pix_val*pix_val;
+            }
+        }
+
+        for (int disp = 0; disp < max_disp; disp++) {
+            float lower_right_sum = 0;
+            long upper_sum = 0;
+            for(int y2 = -window_size ; y2 <= window_size; y2++ ) {
+                for (int x2 = -window_size ; x2 <= window_size ; x2++ ){
+                    int2 coord2 = {x+x2, y+y2};
+                    int l_pix_val = read_imageui(left, sampler, coord2).s0 - l_mean;
+                    int2 coord3 = {x+x2-disp, y+y2};
+                    uint r_mean = read_imageui(right_mean, sampler, coord3).s0;
+                    int r_pix_val = read_imageui(right, sampler, coord3).s0 - r_mean;
+                    lower_right_sum += r_pix_val*r_pix_val;
+                    upper_sum += r_pix_val * l_pix_val;
+                }
+            }
+            float zncc = upper_sum / (sqrt(lower_left_sum)*sqrt(lower_right_sum));
+            if (zncc > best_zncc) {
+                best_disp = disp;
+                best_zncc = zncc;
+            }
+        }
+        uint4 best_pix = {best_disp, best_disp, best_disp, 255};
+        write_imageui(output, coord, best_pix);
+}
