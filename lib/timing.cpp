ad45bc9789e4f0f89f36b6856e368b5a2fcbfa8f
Antti Möttönen <antti.moettoenen@gmail.com>
2017-01-08 17:27:23 +0200

    Fix timer output

--- lib/timing.cpp
+++ lib/timing.cpp
@@ -33,19 +33,19 @@ void Timer::checkPoint(string name) {
 
 string diff(timeval t1, timeval t2) {
     long secs, usecs;
-    long long diff;
-    int million = 1000000;
-    diff = t2.tv_sec * million - t1.tv_sec * million + t2.tv_usec - t1.tv_usec;
+    long diff;
+    long million = 1000000;
+    diff = (t2.tv_sec - t1.tv_sec) * million + t2.tv_usec - t1.tv_usec;
     secs = diff / million;
     usecs = diff - secs * million;
     std::stringstream s = std::stringstream();
-    s << secs << "." << usecs;
+    s << std::setw(2) << secs << "." << std::setfill('0') << std::setw(6) << usecs;
     return s.str();
 }
 
 void Timer::outputDiff(timeStamp t1, timeStamp t2, timeStamp start) {
-    std::cout << std::left << std::setfill('0') << std::setw(8) << diff(t1.time, t2.time) << "s ";
-    std::cout << std::left << std::setfill('0') << std::setw(8) << diff(start.time, t2.time) << "s";
+    std::cout << diff(t1.time, t2.time) << "s ";
+    std::cout << diff(start.time, t2.time) << "s";
     std::cout << std::setfill(' ') << " " << std::setw(30) << t1.name;
     std::cout << std::setw(30) << t2.name << std::endl;
 }
